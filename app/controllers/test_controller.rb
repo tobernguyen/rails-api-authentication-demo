b37d95b6722d56d9528f26be233b6f8ae892b628
Long Nguyen <longnh1994@gmail.com>
2015-09-30 22:25:58 +0700

    Change API routes

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,7 +1,11 @@
-class TestController < ApplicationController
-  before_action :authenticate_user!
+module Api
+  module V1
+    class TestController < ApplicationController
+      before_action :authenticate_user!
 
-  def index
-  	render json: { message: 'Success!' }
+      def index
+        render json: { message: 'Success!' }
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'test/index'
-
-  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  namespace :api do
+    namespace :v1 do
+      get 'test/index'
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+    end
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
